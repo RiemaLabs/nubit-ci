e5745c932af3d5d459063d785ed6e711b1d3a0cf
MeihaoZuyu <anqur@riema.xyz>
2024-05-16 16:28:50 +0800

    fix: exclude some directories during `golangci-lint`
    Signed-off-by: MeihaoZuyu <anqur@riema.xyz>

--- internal/checkers/analyzers/analyzer.go
+++ internal/checkers/analyzers/analyzer.go
@@ -4,15 +4,23 @@ import "github.com/RiemaLabs/nubit-ci/internal/checkers"
 
 const analyzer = "github.com/golangci/golangci-lint/cmd/golangci-lint@latest"
 
+var args = []string{
+	"run",
+	// On GitHub Actions the modcache is analyzed too, that's weird.
+	"--exclude-dirs=(^|/)mod($|/)",
+	"--exclude-dirs-use-default",
+	"--tests",
+}
+
 type Checker struct{}
 
 func (*Checker) Name() string   { return "go-analyze" }
 func (*Checker) Install() error { return nil }
 
 func (*Checker) Check() error {
-	return checkers.Tee(checkers.GoRun(analyzer, "run")).Run()
+	return checkers.Tee(checkers.GoRun(analyzer, args...)).Run()
 }
 
 func (*Checker) Fix() error {
-	return checkers.Tee(checkers.GoRun(analyzer, "run", "--fix")).Run()
+	return checkers.Tee(checkers.GoRun(analyzer, append(args, "--fix")...)).Run()
 }
